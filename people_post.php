420e59cf85f5e977a941e1f61f15c8960b4cae53
henry <hhenry83412@gmail.com>
2015-12-18 14:47:54 +0800

    Create people_post.php

---
+++ people_post.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">   
+</head>
+	<title>活動報名</title>
+	</head>
+	<body>
+	<form action = "activity.php">
+	<h1 align = "center">報名成功!!!!!<h1>
+	<?php
+	echo '<meta http-equiv="refresh" content="10;url=activity.php">';
+	?>
+	<p align = "center">十秒後自動跳轉頁面</p>
+	<input value = "返回活動頁面" type = "submit" align = "center">
+	</form>
+	</body>
+</html>
